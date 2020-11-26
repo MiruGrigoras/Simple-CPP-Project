9e1fec542de53075c4cab5236290b54aafc77ae3
MiruGrigoras <miruna.grig@gmail.com>
2020-11-26 16:58:09 +0200

    Modified for Item 12.

--- src/paint.cpp
+++ src/paint.cpp
@@ -4,22 +4,20 @@
 
 namespace painting{
     WaterBasedPaint::WaterBasedPaint(const WaterBasedPaint &object):Paint(object){};
-    NonWaterBasedPaint::NonWaterBasedPaint(const NonWaterBasedPaint &object):Paint(object), extender(object.extender){};
+    NonWaterBasedPaint::NonWaterBasedPaint(const NonWaterBasedPaint &object):Paint(object), extender(object.extender + "-copyConstr"){};
     AcrylicPaint::AcrylicPaint(const AcrylicPaint &object):NonWaterBasedPaint(object){};
-    OilPaint::OilPaint(const OilPaint &object):NonWaterBasedPaint(object), color(object.color){};
+    OilPaint::OilPaint(const OilPaint &object):NonWaterBasedPaint(object), color(object.color + "-copyConstr"){};
 
     void NonWaterBasedPaint::addTopCoat(){std::cout << "Finished with a varnish coat." << std::endl;}
 
     NonWaterBasedPaint& NonWaterBasedPaint::operator=(const NonWaterBasedPaint &nwbp){
-        NonWaterBasedPaint temp(nwbp);
-        this->extender = temp.extender;
+        this->extender = nwbp.extender + "-copyAssign";
         return *this;
     }
 
     OilPaint& OilPaint::operator=(const OilPaint &op){
-        OilPaint temp(op);
-        NonWaterBasedPaint::operator=(temp);
-        this->color = temp.color;
+        NonWaterBasedPaint::operator=(op);
+        this->color = op.color + "-copyAssign";
         return *this;
     }
 
