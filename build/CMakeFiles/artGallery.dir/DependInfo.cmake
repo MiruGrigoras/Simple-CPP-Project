e9c611d6e312fef54498fd64bd64cbc1cd668f28
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2020-12-10 18:49:48 +0200

    Added Item 14.

---
+++ build/CMakeFiles/artGallery.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rezbanandrei/Desktop/Miruna/iepProject/src/main.cpp" "/Users/rezbanandrei/Desktop/Miruna/iepProject/build/CMakeFiles/artGallery.dir/src/main.cpp.o"
+  "/Users/rezbanandrei/Desktop/Miruna/iepProject/src/paint.cpp" "/Users/rezbanandrei/Desktop/Miruna/iepProject/build/CMakeFiles/artGallery.dir/src/paint.cpp.o"
+  "/Users/rezbanandrei/Desktop/Miruna/iepProject/src/painter.cpp" "/Users/rezbanandrei/Desktop/Miruna/iepProject/build/CMakeFiles/artGallery.dir/src/painter.cpp.o"
+  "/Users/rezbanandrei/Desktop/Miruna/iepProject/src/painting.cpp" "/Users/rezbanandrei/Desktop/Miruna/iepProject/build/CMakeFiles/artGallery.dir/src/painting.cpp.o"
+  "/Users/rezbanandrei/Desktop/Miruna/iepProject/src/surface.cpp" "/Users/rezbanandrei/Desktop/Miruna/iepProject/build/CMakeFiles/artGallery.dir/src/surface.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
